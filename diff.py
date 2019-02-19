839ac42b04497d3e3ef12d8550be7e9855d4599b
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-02-19 05:32:24 +0000

    added file i/o comparison

--- diff.py
+++ diff.py
@@ -2,13 +2,117 @@ import sys
 import os
 import shutil
 import zipfile
+import re
 
 import dump2file
 from bson_parser.windows import *
 
+# Determines if File I/O and Registry I/O in seq1 are a proper subset of seq2
+# API call categories retrieved from: https://github.com/evandowning/monitor/tree/extending-api/sigs/modeling
+def eval_io(seq1, seq2):
+    api_file = list()
+    api_registry = list()
+
+    # Get file api calls
+    with open('api_file.txt','r') as fr:
+        for line in fr:
+            line = line.strip('\n')
+            if line[-1] != 'A':
+                api_file.append(line+'A')
+            if line[-1] != 'W':
+                api_file.append(line+'W')
+
+    # Get registry api calls
+    with open('api_registry.txt','r') as fr:
+        for line in fr:
+            line = line.strip('\n')
+            if line[-1] != 'A':
+                api_registry.append(line+'A')
+            if line[-1] != 'W':
+                api_registry.append(line+'W')
+
+    seq1_file = list()
+    seq1_registry = list()
+
+    seq2_file = list()
+    seq2_registry = list()
+
+    # Extract File I/O & Registry I/O from "before" sequence
+    for k,v in seq1.iteritems():
+        for e in v:
+            call = e['api']
+            print call
+
+            if call in api_file:
+                seq1_file.append(e)
+            elif call in api_registry:
+                seq1_registry.append(e)
+
+    # Extract File I/O & Registry I/O from "after" sequence
+    for k,v in seq2.iteritems():
+        for e in v:
+            call = e['api']
+
+            if call in api_file:
+                seq2_file.append(e)
+            elif call in api_registry:
+                seq2_registry.append(e)
+
+
+    seq1_file_io = list()
+    seq2_file_io = list()
+
+    # Compare File I/O
+    for e in seq1_file:
+        io = [k for k,v in e['arguments'].iteritems() if re.match(r'.*FileName.*', k) is not None]
+        key = e['api'] + ' ' + str([e['arguments'][k] for k in io])
+        seq1_file_io.append(key)
+
+    for e in seq2_file:
+        io = [k for k,v in e['arguments'].iteritems() if re.match(r'.*FileName.*', k) is not None]
+        key = e['api'] + ' ' + str([e['arguments'][k] for k in io])
+
+        if key in seq1_file_io:
+            seq1_file_io.remove(key)
+        else:
+            seq2_file_io.append(key)
+
+    # Print result
+    print 'File I/O results: ("before" should have all api calls in "after")'
+    print '# of files in "after" which are NOT in "before": {0}'.format(len(seq2_file_io))
+    print '# of files in "before" which are NOT in "after": {0}'.format(len(seq1_file_io))
+    print ''
+
+    seq1_registry_io = list()
+    seq2_registry_io = list()
+
+    #TODO - Parse for registry key value arguments
+    # Compare Registry I/O
+    for e in seq1_registry:
+        print e
+        io = [k for k,v in e['arguments'].iteritems() if re.match(r'.*FileName.*', k) is not None]
+        key = e['api'] + ' ' + str([e['arguments'][k] for k in io])
+        seq1_registry_io.append(key)
+
+    for e in seq2_registry:
+        print e
+        io = [k for k,v in e['arguments'].iteritems() if re.match(r'.*FileName.*', k) is not None]
+        key = e['api'] + ' ' + str([e['arguments'][k] for k in io])
+
+        if key in seq1_registry_io:
+            seq1_registry_io.remove(key)
+        else:
+            seq2_registry_io.append(key)
+
+    # Print result
+    print 'Registry I/O results: ("before" should have all api calls in "after")'
+    print '# of files in "after" which are NOT in "before": {0}'.format(len(seq2_registry_io))
+    print '# of files in "before" which are NOT in "after": {0}'.format(len(seq1_registry_io))
+    print ''
+
+
 # Determines if seq1 is a proper subset of seq2
 def eval_seq(seq1, seq2):
-
     api2 = list()
     api1 = list()
 
@@ -31,9 +135,10 @@ def eval_seq(seq1, seq2):
                 api1.append(call)
 
     # Print result
-    print 'Sequence results: (before should have all api calls in after)'
-    print '# of API calls in after which are NOT in before: {0}'.format(len(api2))
-    print '# of API calls in before which are NOT in after: {0}'.format(len(api1))
+    print 'Sequence results: ("before" should have all api calls in "after")'
+    print '# of API calls in "after" which are NOT in "before": {0}'.format(len(api2))
+    print '# of API calls in "before" which are NOT in "after": {0}'.format(len(api1))
+    print ''
 
 # Extracts API calls from raw Cuckoo logs
 def extract(dump_fn):
@@ -119,8 +224,8 @@ def _main():
     # Evaluate sequence
     eval_seq(api_before,api_after)
 
-    #TODO
     # Evaluate File I/O and Registry I/O
+    eval_io(api_before,api_after)
 
 if __name__ == '__main__':
     _main()
